3772a72c0b906510d198321750509e2d9c5c479f
Rommel Rico <me@rommelrico.com>
2019-11-07 19:09:23 -0800

    :sparkles: NewPlaceScreen: Scaffolding

---
+++ screens/NewPlaceScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from "react-native";
+
+const NewPlaceScreen = (props) => {
+  return (
+    <View>
+      <Text>New Place Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default NewPlaceScreen;
