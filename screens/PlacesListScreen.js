1a9648b44cba41c7845a6d0f06d0417585027881
Rommel Rico <me@rommelrico.com>
2019-11-07 19:24:42 -0800

    :sparkles: PlacesListScreen: Navigating button

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { View, Text, StyleSheet, Platform } from "react-native";
+import CustomHeaderButton from "../components/CustomHeaderButton";
 
 const PlacesListScreen = (props) => {
   return (
@@ -9,8 +11,24 @@ const PlacesListScreen = (props) => {
   );
 };
 
-PlacesListScreen.navigationOptions = {
-  headerTitle: "All Places"
+PlacesListScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: "All Places",
+    headerRight: (
+      <HeaderButtons
+        title="header-right"
+        HeaderButtonComponent={CustomHeaderButton}
+      >
+        <Item
+          title="Add Place"
+          iconName={Platform.OS === "android" ? "md-add" : "ios-add"}
+          onPress={() => {
+            navData.navigation.navigate("NewPlace");
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
 };
 
 const styles = StyleSheet.create({});
