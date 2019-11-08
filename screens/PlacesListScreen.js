f0293aa71a16b1608d17aa3e439bb51464e7e937
Rommel Rico <me@rommelrico.com>
2019-11-07 19:08:20 -0800

    :sparkles: PlacesListScreen: Scaffolding

---
+++ screens/PlacesListScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from "react-native";
+
+const PlacesListScreen = (props) => {
+  return (
+    <View>
+      <Text>PlacesListScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default PlacesListScreen;
