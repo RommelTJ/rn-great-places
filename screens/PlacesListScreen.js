46e443bf6f4e7841a346b49aa4d817ae4febc81f
Rommel Rico <me@rommelrico.com>
2019-11-09 14:34:01 -0800

    :sparkles: PlacesListScreen: Implementing the PlaceListScreen FlatList

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
-import { View, Text, StyleSheet, Platform } from "react-native";
+import { View, Text, StyleSheet, Platform, FlatList } from "react-native";
+import { useSelector } from "react-redux";
 import CustomHeaderButton from "../components/CustomHeaderButton";
+import PlaceItem from "../components/PlaceItem";
 
 const PlacesListScreen = (props) => {
+  const places = useSelector(state => state.places.places);
   return (
-    <View>
-      <Text>PlacesListScreen</Text>
-    </View>
+    <FlatList
+      data={places}
+      keyExtractor={item => item.id}
+      renderItem={itemData => {
+        return (
+          <PlaceItem
+            title={itemData.item.title}
+            address={null}
+            image={null}
+            onSelect={() => {
+              props.navigation.navigate(
+                "PlaceDetail",
+                {
+                  placeTitle: itemData.item.title,
+                  placeId: itemData.item.id
+                }
+              );
+            }}
+          />
+        );
+      }}
+    />
   );
 };
 
