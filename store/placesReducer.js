2efbefca855648d5bc3e81f1a93477d6e1179ee4
Rommel Rico <me@rommelrico.com>
2019-11-09 08:24:29 -0800

    :sparkles: placesReducer: Implementing add place reducer

--- store/placesReducer.js
+++ store/placesReducer.js
@@ -1,9 +1,15 @@
+import {ADD_PLACE} from "./placesActions";
+import Place from "../models/place";
+
 const initialState = {
   places: []
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case ADD_PLACE:
+      const place = new Place(new Date().toString(), action.placeData.title);
+      return {places: state.places.concat(place)};
     default:
       return state;
   }
