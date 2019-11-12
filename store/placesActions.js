b02c8f4803a3ab6a8141f8c8f6b4b573b5814149
Rommel Rico <me@rommelrico.com>
2019-11-12 06:57:49 -0800

    :sparkles: Store: Adding image to addPlace action

--- store/placesActions.js
+++ store/placesActions.js
@@ -1,5 +1,5 @@
 export const ADD_PLACE = "ADD_PLACE";
 
-export const addPlace = (title) => {
-  return { type: ADD_PLACE, placeData: { title } };
+export const addPlace = (title, image) => {
+  return { type: ADD_PLACE, placeData: { title, image } };
 };
