b253799385a16b0f6117a97621d39d7517041051
Rommel Rico <me@rommelrico.com>
2019-11-11 21:00:16 -0800

    :sparkles: ImgPicker: Getting access to the image

--- components/ImgPicker.js
+++ components/ImgPicker.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Button, Text, StyleSheet, Image, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from "expo-permissions";
 import Colors from "../constants/Colors";
 
 const ImgPicker = (props) => {
+  const [pickedImage, setPickedImage] = useState(undefined);
 
   const verifyPermissions = async () => {
     const result = await Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL);
@@ -22,18 +23,22 @@ const ImgPicker = (props) => {
   const takeImageHandler = async () => {
     const hasPermission = await verifyPermissions();
     if (!hasPermission) return;
-    ImagePicker.launchCameraAsync({
+    const image = await ImagePicker.launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5
     });
+    setPickedImage(image.uri);
   };
 
   return (
     <View style={styles.imagePicker} >
       <View style={styles.imagePreview}>
-        <Text>No image picked yet.</Text>
-        <Image style={styles.image} />
+        {
+          !pickedImage
+            ? <Text>No image picked yet.</Text>
+            : <Image style={styles.image} source={{uri: pickedImage}} />
+        }
       </View>
       <Button title={"Take Image"} color={Colors.primary} onPress={takeImageHandler} />
     </View>
