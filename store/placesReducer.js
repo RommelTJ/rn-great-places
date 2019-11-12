aa048f1927ae7bc28130d8c8c25ef631bede1d82
Rommel Rico <me@rommelrico.com>
2019-11-12 07:00:08 -0800

    :sparkles: Store: Handling place with image in reducer

--- store/placesReducer.js
+++ store/placesReducer.js
@@ -8,7 +8,7 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_PLACE:
-      const place = new Place(new Date().toString(), action.placeData.title);
+      const place = new Place(new Date().toString(), action.placeData.title, action.placeData.image);
       return {places: state.places.concat(place)};
     default:
       return state;
