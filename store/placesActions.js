5f9bdc1ebaa06d58292f8f4be29d6a40bb41e0c7
Rommel Rico <me@rommelrico.com>
2019-11-13 07:10:17 -0800

    :sparkles: Store: Storing images in a permanent directory

--- store/placesActions.js
+++ store/placesActions.js
@@ -1,5 +1,18 @@
+import * as FileSystem from 'expo-file-system';
+
 export const ADD_PLACE = "ADD_PLACE";
 
 export const addPlace = (title, image) => {
-  return { type: ADD_PLACE, placeData: { title, image } };
+  return async (dispatch) => {
+    const fileName = image.split('/').pop();
+    const newPath = FileSystem.documentDirectory + fileName;
+    try {
+      await FileSystem.moveAsync({from: image, to: newPath});
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+    return { type: ADD_PLACE, placeData: { title, newPath } };
+  };
+
 };
