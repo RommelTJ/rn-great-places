7f652911a1ef161014d8a89fbfe94ac63f711654
Rommel Rico <me@rommelrico.com>
2019-11-15 07:27:26 -0800

    :sparkles: Store: Using the correct ID in the reducer

--- store/placesReducer.js
+++ store/placesReducer.js
@@ -8,7 +8,11 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_PLACE:
-      const place = new Place(new Date().toString(), action.placeData.title, action.placeData.image);
+      const place = new Place(
+        action.placeData.id.toString(),
+        action.placeData.title,
+        action.placeData.image
+      );
       return {places: state.places.concat(place)};
     default:
       return state;
