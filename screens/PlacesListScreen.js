8fbc9f2aff4f59e266b014f50b81a24f4b8f32da
Rommel Rico <me@rommelrico.com>
2019-11-15 19:26:36 -0800

    :sparkles: PlacesListScreen: Dispatching loadPlaces

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { View, Text, StyleSheet, Platform, FlatList } from "react-native";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import CustomHeaderButton from "../components/CustomHeaderButton";
 import PlaceItem from "../components/PlaceItem";
+import * as placesActions from "../store/placesActions";
 
 const PlacesListScreen = (props) => {
   const places = useSelector(state => state.places.places);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(placesActions.loadPlaces());
+  }, [dispatch]);
+
   return (
     <FlatList
       data={places}
