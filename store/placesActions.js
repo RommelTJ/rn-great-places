ff6f33e66bcdeabb26c7d4d211bf9a96ed71d95e
Rommel Rico <me@rommelrico.com>
2019-11-15 19:26:21 -0800

    :sparkles: Store: Setting the places in the action creator

--- store/placesActions.js
+++ store/placesActions.js
@@ -1,7 +1,8 @@
 import * as FileSystem from 'expo-file-system';
-import { insertPlace } from "../helpers/db";
+import { insertPlace, fetchPlaces } from "../helpers/db";
 
 export const ADD_PLACE = "ADD_PLACE";
+export const SET_PLACES = "SET_PLACES";
 
 export const addPlace = (title, image) => {
   return async (dispatch) => {
@@ -16,5 +17,15 @@ export const addPlace = (title, image) => {
       throw err;
     }
   };
+};
 
+export const loadPlaces = () => {
+  return async (dispatch) => {
+    try {
+      const dbResult = await fetchPlaces();
+      dispatch({ type: SET_PLACES, places: dbResult.rows._array });
+    } catch (err) {
+      throw err;
+    }
+  }
 };
