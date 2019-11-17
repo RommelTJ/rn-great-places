30776e6450e2b1a74ddc86fefb0e17b373b4da0b
Rommel Rico <me@rommelrico.com>
2019-11-17 12:01:38 -0800

    :sparkles: Models: Adding address, lat, lon to Place model

--- models/place.js
+++ models/place.js
@@ -1,8 +1,11 @@
 class Place {
-  constructor(id, title, imageUri) {
+  constructor(id, title, imageUri, address, lat, lon) {
     this.id = id;
     this.title = title;
     this.imageUri = imageUri;
+    this.address = address;
+    this.lat = lat;
+    this.lon = lon;
   }
 }
 
