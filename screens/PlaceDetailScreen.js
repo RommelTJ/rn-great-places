3e91518fa5d276e099b84ca59272f9a32c470ef7
Rommel Rico <me@rommelrico.com>
2019-11-17 12:11:01 -0800

    :sparkles: PlaceDetailScreen: Creating this view

--- screens/PlaceDetailScreen.js
+++ screens/PlaceDetailScreen.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
+import { useSelector } from "react-redux";
+import MapPreview from "../components/MapPreview";
 
 const PlaceDetailScreen = (props) => {
+  const placeId = props.navigation.getParam("placeId");
+  const selectedPlace = useSelector((state) => state.places.places.find(p => p.id === placeId));
+
   return (
-    <View>
-      <Text>Place Detail Screen</Text>
-    </View>
+    <ScrollView>
+      <Image source={{uri: selectedPlace.imageUri}} />
+      <View>
+        <View>
+          <Text>{selectedPlace.address}</Text>
+        </View>
+        <MapPreview location={{lat: selectedPlace.lat, lon: selectedPlace.lon}} />
+      </View>
+    </ScrollView>
   );
 };
 
