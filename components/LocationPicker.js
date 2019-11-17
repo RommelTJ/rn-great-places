f41269b177fe480e080e3b5b9da2f875e5772b3a
Rommel Rico <me@rommelrico.com>
2019-11-17 11:29:57 -0800

    :sparkles: LocationPicker: Getting the selected location if we have it

--- components/LocationPicker.js
+++ components/LocationPicker.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, Button, Text, ActivityIndicator, Alert, StyleSheet } from 'react-native';
 import * as Location from 'expo-location';
 import * as Permissions from "expo-permissions";
@@ -8,6 +8,11 @@ import MapPreview from "./MapPreview";
 const LocationPicker = (props) => {
   const [pickedLocation, setPickedLocation] = useState(undefined);
   const [isFetching, setIsFetching] = useState(false);
+  const mapPickedLocation = props.navigation.getParam('pickedLocation');
+
+  useEffect(() => {
+    if (mapPickedLocation) setPickedLocation(mapPickedLocation);
+  }, [mapPickedLocation]);
 
   const verifyPermissions = async () => {
     const result = await Permissions.askAsync(Permissions.LOCATION);
