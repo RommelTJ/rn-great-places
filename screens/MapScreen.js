3450d076e85a4525a68524ee8cb613a81ce9890f
Rommel Rico <me@rommelrico.com>
2019-11-16 18:20:48 -0800

    :sparkles: MapScreen: Picking a place

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import MapView from 'react-native-maps';
+import React, { useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet } from 'react-native';
 
 const MapScreen = (props) => {
+  const [selectedLocation, setSelectedLocation] = useState(undefined);
+
   const mapRegion = {
     latitude: 37.78,
     longitude: -122.43,
@@ -10,7 +12,24 @@ const MapScreen = (props) => {
     longitudeDelta: 0.0421
   };
 
-  return <MapView style={styles.map} region={mapRegion}/>;
+  const selectLocationHandler = (event) => {
+    setSelectedLocation({
+      lat: event.nativeEvent.coordinate.latitude,
+      lon: event.nativeEvent.coordinate.longitude
+    })
+  };
+
+  let markerCoordinates;
+  if (selectedLocation) markerCoordinates = {
+    latitude: selectedLocation.lat,
+    longitude: selectedLocation.lon
+  };
+
+  return (
+    <MapView style={styles.map} region={mapRegion} onPress={selectLocationHandler} >
+      {markerCoordinates && <Marker title="Picked Location" coordinate={markerCoordinates} />}
+    </MapView>
+  );
 };
 
 const styles = StyleSheet.create({
