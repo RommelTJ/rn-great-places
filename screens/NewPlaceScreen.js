8dc4b37f2762f2cd09ca3049fc85882ce56ec340
Rommel Rico <me@rommelrico.com>
2019-11-18 21:59:56 -0800

    :sparkles: NewPlaceScreen: Dummy change to demo OTA updates

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -44,7 +44,7 @@ const NewPlaceScreen = (props) => {
 };
 
 NewPlaceScreen.navigationOptions = {
-  headerTitle: "Add Place"
+  headerTitle: "Add New Place"
 };
 
 const styles = StyleSheet.create({
